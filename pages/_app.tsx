db4ba1429b73a954eb3caed565889af7960c5ad7
suzz-in <o_o_su_jin_@naver.com>
2023-02-02 17:55:31 +0900

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+// import App from "next/app";
+import type { AppProps /*, AppContext */ } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { useState } from 'react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+      // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
+    const [queryClient] = useState(() => new QueryClient());
+
+  return (    
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  </QueryClientProvider>)
+}
+
+export default MyApp;
