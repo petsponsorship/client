6539b3baa68e9ab282771acf3a382ac317e3b699
suzz-in <o_o_su_jin_@naver.com>
2023-02-09 18:45:28 +0900

    design : mainpage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import styles from '../styles/Home.module.css';
 
 function Main() {
-  return <div>메인 !</div>;
+  // const btnRef = useRef();
+  // let btn = getElementsByClassName('btn');
+  // const handleClick = (e) => {
+  //   if (e.target.classList[1] === 'clicked') {
+  //     e.target.classList.remove('clicked');
+  //   } else {
+  //     for (let i = 0; i < btnRef.length; i++) {
+  //       btnRef[i].classList.remove('clicked');
+  //     }
+  //     e.target.classList.add('clicked');
+  //   }
+  // };
+
+  // const init = () => {
+  //   for (let i = 0; i < btnRef.length; i++) {
+  //     btnRef[i].addEventListener('click', handleClick);
+  //   }
+  // };
+
+  // init();
+  return (
+    <>
+      <div className={styles.adbox}>슬라이드</div>
+      <div className={styles.categorybox}>
+        <ul className={styles.list}>
+          <li>
+            <span className={styles.categorybtn} ref={btnRef}>
+              강아지
+            </span>
+          </li>
+          <li>
+            <span className={styles.categorybtn} ref={btnRef}>
+              고양이
+            </span>
+          </li>
+          <li>
+            <span className={styles.categorybtn} ref={btnRef}>
+              기타
+            </span>
+          </li>
+        </ul>
+        <hr className={styles.line} />
+      </div>
+    </>
+  );
 }
 
 export default Main;
