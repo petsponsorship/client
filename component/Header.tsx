81c3d2e89ed8961f234c3fea5b357d10d482cdce
suzz-in <o_o_su_jin_@naver.com>
2023-02-14 16:01:23 +0900

    design : header LINK 수정

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styles from "../styles/Header.module.css"
-
+import Link from 'next/link'
 
 function Header (){
     return (<header className={styles.container}>
-    <h1 className={styles.title}>댕도네냥</h1>
-    {/* 아이콘 갖다쓸려하는데 react-icon / mui 뭐가 좋을까 */}
+    <Link href="/" className={styles.title}>
+        댕도네냥
+        </Link>
     <div className={styles.btnbox}>
     <button className={styles.bellbtn}>🔔</button>
-    <button className={styles.loginbtn}>로그인</button>
+    <Link href="/login" className={styles.loginbtn}>
+        <button className={styles.loginbtn}>
+            로그인
+            </button>
+            </Link>
     <button className={styles.postbtn}>글쓰기</button>
     </div>
     </header>)
