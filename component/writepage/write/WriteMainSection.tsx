db34bea4527968faf45b69f7195461dc4038eca1
hyein <poptarts@kakao.com>
2023-02-19 16:50:36 +0900

    design: 상세페이지 및 작성페이지 레이아웃 수정, input 조건 추가

--- component/writepage/write/WriteMainSection.tsx
+++ component/writepage/write/WriteMainSection.tsx
@@ -2,31 +2,32 @@ import React from 'react';
 import styles from '../write/WriteMainSection.module.css';
 
 function WriteMainSection() {
+  const today = new Date().toISOString().slice(0, 10);
+
   return (
     <main className={styles.container}>
-      <div>
+      <section>
         <img
-          alt="대표 이미지"
           className={styles.thumbnail}
           src="https://www.wooripet.co.kr/data/file/come3/thumb-1948775479_b24FmajJ_EF1D7C03-7C6C-4B6C-8C4E-C364005AFA68-74120D00-ED9F-463E-8221-7E53CEBBDE93_1400x933.jpg"
         />
         <div>
-          <input type="file" />
+          <input type="file" accept="image/*" />
         </div>
-      </div>
+      </section>
       <section className={styles.rightsection}>
         <div className={styles.flexdiv}>
           <p>등록일</p>
-          <p>2023. 2. 15</p>
+          <input type="date" value={today} readOnly />
           <span>❗️ 후원은 등록일로부터 2주간 유지됩니다</span>
         </div>
         <div className={styles.flexdiv}>
           <p>이름</p>
-          <input type="text" />
+          <input type="text" maxLength={10} />
         </div>
         <div className={styles.flexdiv}>
           <p>나이</p>
-          <input type="number" />
+          <input type="number" max={30} />
           <span>
             <label>
               <input type="radio" name="age" value="month" />
@@ -37,7 +38,7 @@ function WriteMainSection() {
               만, 세
             </label>
             <label>
-              <input type="radio" name="age" value="unknown" />
+              <input type="radio" name="age" value={-1} />
               나이 모름
             </label>
           </span>
@@ -48,8 +49,9 @@ function WriteMainSection() {
             <option>종을 선택해주세요!</option>
             <option value="dog">강아지</option>
             <option value="cat">고양이</option>
-            <option value="etc">기타</option>
+            <option value="etc">직접 입력</option>
           </select>
+          <input disabled />
         </div>
         <div className={styles.flexdiv}>
           <p>성별</p>
@@ -67,23 +69,22 @@ function WriteMainSection() {
         </div>
         <div className={styles.flexdiv}>
           <p>목표 금액</p>
-          <input type="number" name="goal" />
+          <input type="number" name="goal" max={500} />
+          <span>만 원</span>
         </div>
         <div className={styles.flexdiv}>
           <p>후원 목적</p>
           <select name="purpose">
             <option>후원목적을 선택해주세요!</option>
             <option value="의료비">의료비</option>
-            <option value="식비">식비</option>
-            <option value="기타">기타</option>
+            <option value="식비">사료 및 간식</option>
+            <option value="기타">용품비</option>
+            <option value="기타">장례비</option>
           </select>
           <label>
             <input type="checkbox" name="neutering" value="true" /> 입양문의 받기
           </label>
         </div>
-        <section className={styles.btnSection}>
-          <button>작성 완료</button>
-        </section>
       </section>
     </main>
   );
