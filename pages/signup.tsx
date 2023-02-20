9c6c68a0a6011dd02e0052ba365d394067810170
suzz-in <o_o_su_jin_@naver.com>
2023-02-20 15:24:26 +0900

    design : 회원가입

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "../styles/Signup.module.css"
 import useSignForm from "../hook/useSignForm.js"
-import axios from "axios";
 import { signUpApi } from "../apis/auth";
 
 function signup() {
@@ -35,24 +34,23 @@ function signup() {
     <section className={styles.signupcontainer}>
         <h1>이메일 간편가입</h1>
         <form className={styles.signupform} onSubmit={handleSignupClick}>
-            <label>이메일</label>
-            <div>
-                <input className={styles.email} type="text" onChange={handleInputValue("email")}/>
-                <button className={styles.emailcheck}>인증하기</button>
-            </div>
-            <label>이름</label>
-            <input type="text" onChange={handleInputValue("name")}/>
-            <label>비밀번호</label>
-            <input type="text" onChange={handleInputValue("password")}/>
-            <label>휴대폰번호</label>
-            <input  type="text" onChange={handleInputValue("phoneNumber")}/>
-            <button>완료</button>
-            <div>        {emailWarnList?.map((item) => (
-          <div key={item}>{item}</div>
-        ))}
-        {passwordWarnList?.map((item) => (
-          <div key={item}>{item}</div>
-        ))}</div>
+            <input className={styles.textinput} placeholder="Email" type="text"  onChange={handleInputValue("email")}/>
+            <div className={styles.warntext}>{emailWarnList?.map((item) => (
+                <div key={item}>{item}</div>
+               ))}</div>
+            <input className={styles.textinput} placeholder="이름" type="text" onChange={handleInputValue("name")}/>
+            <div className={styles.warntext}>{nameWarnList?.map((item) => (
+                <div key={item}>{item}</div>
+               ))}</div>
+            <input className={styles.textinput} placeholder="비밀번호" type="password" onChange={handleInputValue("password")}/>
+            <div className={styles.warntext}>{passwordWarnList?.map((item) => (
+                <div key={item}>{item}</div>
+               ))}</div>
+            <input  className={styles.textinput} placeholder="휴대폰번호" type="text" onChange={handleInputValue("phoneNumber")}/>
+            <div className={styles.warntext}>{phoneNumberWarnList?.map((item) => (
+                <div key={item}>{item}</div>
+               ))}</div>
+            <button className={styles.signupbtn}>회원가입완료</button>
         </form>
     </section>)
 }
