ac76e8bc3631e45f045bdd4294cdc218843c870e
suzz-in <o_o_su_jin_@naver.com>
2023-03-01 21:32:25 +0900

    feat : 후원금액단위 수정

--- component/mainpage/card/Card.tsx
+++ component/mainpage/card/Card.tsx
@@ -7,6 +7,7 @@ import { priceForm } from "../../../util/priceForm"
 
 
 function Card({list}) {
+    console.log("카드데이터", list)
 
     const speciesIcon = (species) => {
         if( species === "강아지") {
@@ -38,7 +39,7 @@ const progressbarAmount = (targetAmount, amount) => {
             <span className={styles.participation}>{list.sponsor}명 참여</span>
             <span className={styles.period}> {dateForm(list.createdAt)}일 남음</span>
             <ProgressBar value={progressbarAmount(list.targetAmount, list.amount)}/>
-            <span className={styles.amount}>{priceForm(list.targetAmount)}원</span>
+            <span className={styles.amount}>{priceForm(list.targetAmount)} 만원</span>
         </div>
         <div className={styles.infobox}>
             <p className={styles.info}>{list.sex === 0 ? "남아" : "여아"}&nbsp;&nbsp;</p>
--- pages/index.js
+++ pages/index.js
@@ -56,47 +56,47 @@ function Main({ data }) {
   //응원순
 
   return (
-    // <Suspense fallback={<div>Loading...</div>}>
-    <article>
-      <div className={styles.adbox}>슬라이드</div>
-      <section>
-        <CategoryTag category={category} onSelect={onSelect} />
-        <div className={styles.selectDiv}>
-          <h2 className={styles.filterTitle}>{category}</h2>
-          <div>
-            <input type="checkbox" />
-            <span>입양 가능한 아이만 볼래요!</span>
+    <Suspense fallback={<div>Loading...</div>}>
+      <article>
+        <div className={styles.adbox}>슬라이드</div>
+        <section>
+          <CategoryTag category={category} onSelect={onSelect} />
+          <div className={styles.selectDiv}>
+            <h2 className={styles.filterTitle}>{category}</h2>
+            <div>
+              <input type="checkbox" />
+              <span>입양 가능한 아이만 볼래요!</span>
+            </div>
+            <ul className={styles.selectlist}>
+              <li>
+                <span
+                  className={filterState === 'imminent' ? styles.isSelected : styles.select}
+                  onClick={() => imminentFilter()}>
+                  마감임박순
+                </span>
+              </li>
+              <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+              <li>
+                <span
+                  className={filterState === 'newest' ? styles.isSelected : styles.select}
+                  onClick={() => newestFilter()}>
+                  최신순
+                </span>
+              </li>
+              <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+              <li>
+                <span className={styles.select}> 응원순</span>
+              </li>
+            </ul>
           </div>
-          <ul className={styles.selectlist}>
-            <li>
-              <span
-                className={filterState === 'imminent' ? styles.isSelected : styles.select}
-                onClick={() => imminentFilter()}>
-                마감임박순
-              </span>
-            </li>
-            <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-            <li>
-              <span
-                className={filterState === 'newest' ? styles.isSelected : styles.select}
-                onClick={() => newestFilter()}>
-                최신순
-              </span>
-            </li>
-            <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-            <li>
-              <span className={styles.select}> 응원순</span>
-            </li>
-          </ul>
-        </div>
-      </section>
-      <section className={styles.cardsection}>
-        {cards?.map((card) => (
-          <Card key={card.id} list={card} />
-        ))}
-      </section>
-    </article>
-    // </Suspense>
+        </section>
+        <section className={styles.cardsection}>
+          {cards?.map((card) => (
+            <Card key={card.id} list={card} />
+          ))}
+        </section>
+      </article>
+    </Suspense>
   );
 }
 
