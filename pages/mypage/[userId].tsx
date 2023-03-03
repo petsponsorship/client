df6aca3be3b931ce66038db6b765a04b9fce55e4
suzz-in <o_o_su_jin_@naver.com>
2023-03-03 17:02:59 +0900

    feat: 전체 데이터 api콜

--- pages/mypage/[userId].tsx
+++ pages/mypage/[userId].tsx
@@ -1,19 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getMySupportListApi, getMylikeListApi, getMyWriteListApi } from "../../apis/mypage";
 import { parseCookies } from "../../helpers"
 import { getCookie } from "../../hook/cookies";
 import styles from "../../styles/Mypage.module.css"
 
+  //1. 전역관리를 한다
+  //useRecoilState 3개 생성해서 api 콜 할때마다 setState 저장. 버튼 누를때마다 useRecoilState값을 React.useState에 넣어줌
+  
+  //2.api콜
+  //첫 랜더링시 3개 api 콜 모두함.
+  //후원한 게시글만 setState 사용해서 state에 저장함
+  //버튼 누를때마다 api콜 state 바꿔줌
+
 function Mypage () {
   const userId = getCookie("userId");
+  const [cardData, setCardData] = useState([]);
+
+
   console.log(userId);
   const getAllData = async () => {
     getMyWriteListApi(userId).then((res)=>console.log("내가작성한글 -->", res.data))
     getMySupportListApi().then((res)=>console.log("내가 후원한 리스트-->", res.data.supportList))
     getMylikeListApi().then((res)=>console.log("내가좋아요한 리스트-->", res))
   }
+
+
+
+  const getWriteList =() =>{
+    getMyWriteListApi(userId).then((res)=>
+    setCardData(res.data)
+    )
+  }
+
+  const getSupportList = () => {
+    getMySupportListApi().then((res)=>
+    console.log("내가 후원한 리스트-->", res.data.supportList))
+  }
+
+  const getLiktList = () => {
+    getMylikeListApi().then((res)=>
+    console.log("내가좋아요한 리스트-->", res))
+  }
   useEffect(()=>{
     getAllData();
+    getWriteList();
   },[])
 
     return(<section className={styles.mypageSection}>
@@ -28,7 +58,6 @@ function Mypage () {
           <p className={styles.price}>0</p>
           <span className={styles.ment}>후원한</span>
           </div>
-          {/* <div className={styles.divBar}/> */}
 
           <div className={styles.pricebox}>
           <p className={styles.price}>0</p>
