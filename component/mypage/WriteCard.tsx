ba8b1ca055e2fa5b04f1dfc5c7676e85e5f30661
suzz-in <o_o_su_jin_@naver.com>
2023-03-22 12:35:53 +0900

    feat: 후원종료

--- component/mypage/WriteCard.tsx
+++ component/mypage/WriteCard.tsx
@@ -7,33 +7,40 @@ import { putPostEndApi } from "../../apis/mypage";
 
 
 function WriteCard ({card}) {
-    console.log("작성카드",card)
+
+    const {thumbnail, name, age, sex, sponsor, targetAmount, amount, id } = card;
+
+
 
     const sponsorEnd = async(id: number) => {
-        confirm("후원을 종료하시겠습니까?")
-       return await putPostEndApi(id)
+        if(confirm("후원을 종료하시겠습니까?")){
+            return await putPostEndApi(id).then((res)=> console.log(res))
+        } else{
+            return; 
+        }
+      
     }
 
 
     return(
         <Link href={{
-            pathname: `/post/${card.id}`
+            pathname: `/post/${id}`
         }}>
     <div className={styles.container}>
-        <Image alt="대표사진gg" src={card.thumbnail} width={210} height={200} className={styles.img}/>
+        <Image alt="대표사진gg" src={thumbnail} width={210} height={200} className={styles.img}/>
         <div  className={styles.contentDiv}>
-            <div  className={styles.name}>
+            <div  className={name}>
             <h2>{card.name}</h2>
-               <span className={styles.detail}>{card.age}세, {card.sex === 0 ? "남아" : "여아"}</span>
+               <span className={styles.detail}>{age}세, {sex === 0 ? "남아" : "여아"}</span>
             </div>
-        <span className={styles.participant}>{card.sponsor}명 참여</span>
+        <span className={styles.participant}>{sponsor}명 참여</span>
 
-        <ProgressBar value={progressbarAmount(card.targetAmount, card.amount)}/>
+        <ProgressBar value={progressbarAmount(targetAmount, amount)}/>
 
-        <span className={styles.price}>{card.targetAmount} 만원</span>
+        <span className={styles.price}>{targetAmount} 만원</span>
 
         <div className={styles.btnbox}>
-            <button className={styles.endbtn}>후원 종료하기</button>
+            <button className={styles.endbtn} onClick={()=>sponsorEnd(id)}>후원 종료하기</button>
             <button className={styles.extendbtn}>후원 연장하기</button>
         </div>
         </div>
